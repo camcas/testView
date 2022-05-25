19bbfae4576b0c0d226d7b67b376efa6ecb4d5b9
camilo <camilo.castillo@gopass.com.co>
2022-05-25 11:28:07 -0500

    Updates

---
+++ precache-manifest.cdcd7ffc14f72cf48d9f49c104875a54.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "781a47377e57ec078f50",
+    "url": "/testView/static/js/main.781a4737.chunk.js"
+  },
+  {
+    "revision": "dc77a65fb024aa5f5e45",
+    "url": "/testView/static/js/1.dc77a65f.chunk.js"
+  },
+  {
+    "revision": "2b9d737540bebfc1b9bf",
+    "url": "/testView/static/js/runtime~main.2b9d7375.js"
+  },
+  {
+    "revision": "3d9644ef0ce488a50656e3797716b573",
+    "url": "/testView/index.html"
+  }
+];
